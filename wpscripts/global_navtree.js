351258cbb161fd23f4c6f983a4d2f03ef2b45dc5
Nicholas Eckardt <neckardt@umich.edu>
2017-11-24 16:41:09 -0500

    automatic push from yoshiko

---
+++ wpscripts/global_navtree.js
@@ -0,0 +1,23 @@
+var global_navtree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+    'bIsWebPath':true,
+    'sUrl':'index.html'
+},
+{   'sTitle':'Services',
+    'bIsWebPath':true,
+    'sUrl':'page2.html'
+},
+{   'sTitle':'Tools',
+    'bIsWebPath':true,
+    'sUrl':'page3.html'
+},
+{   'sTitle':'Clients',
+    'bIsWebPath':true,
+    'sUrl':'page4.html'
+},
+{   'sTitle':'Contact',
+    'bIsWebPath':true,
+    'sUrl':'page5.html'
+}]
+});
